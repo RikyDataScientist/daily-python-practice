ecb19032c7f840c1ce0db71b3bd01bfe7ecca414
Faris Riky Pratama <farisriky01@gmail.com>
2025-10-23 05:40:30 +0700

    Update day-02.py

--- exercises/day-02.py
+++ exercises/day-02.py
@@ -8,7 +8,7 @@
 Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
 💡 Example:
->>> 145263
+>>> descending_order(145263)
 654321
 """
 
