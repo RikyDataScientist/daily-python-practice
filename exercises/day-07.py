6f20c512e75cf0ffcb45cb1bf2ac01bbc50c6377
Faris Riky Pratama <farisriky01@gmail.com>
2025-10-27 21:48:09 +0700

    Create day-07.py

---
+++ exercises/day-07.py
@@ -0,0 +1,34 @@
+"""
+🧠 Challenge: Duplicate Arguments
+🔗 Link: https://www.codewars.com/kata/520d9c27e9940532eb00018e
+🏷️ Level: 6
+📅 Date: 2025-10-27
+
+📝 Instruction:
+Complete the solution so that it returns true if it contains any duplicate argument values, and false otherwise. Any number of arguments may be passed into the function.
+
+The arguments passed in will only be strings or numbers.
+
+💡 Example:
+>>> solution(1, 2, 3)
+False
+>>> solution(1, 2, 3, 2)
+True
+"""
+
+# ✨ Your Solution
+def solution(*n):
+    for i in n:
+        if n.count(i) > 1:
+            return True
+    return False
+
+
+# ✅ Test Cases
+if __name__ == "__main__":
+    print(solution(1, 2, 3, 3, 4))  # True
+
+# 📝 Note
+"""
+I still my best performance to train my logis to get clean, efficient, and readable code.
+"""
