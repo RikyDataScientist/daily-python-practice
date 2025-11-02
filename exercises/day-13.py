3f922ab75509cd6faa0f0ec34156144721b379a7
Faris Riky Pratama <farisriky01@gmail.com>
2025-11-02 10:02:33 +0700

    Add practice for 13th day.

---
+++ exercises/day-13.py
@@ -0,0 +1,27 @@
+"""
+🧠 Challenge: MakeUpperCase
+🔗 Link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+🏷️ Level: 8 kyu
+📅 Date: 2025-11-02
+
+📝 Instruction:
+Write a function which converts the input string to uppercase.
+
+💡 Example:
+>>> make_upper_case('I love her')
+I LOVE HER
+"""
+
+# ✨ Your Solution
+def make_upper_case(s):
+    return s.upper()
+
+
+# ✅ Test Cases
+if __name__ == "__main__":
+    print(make_upper_case("It's so embarassed me"))  # IT'S SO EMBARASSED ME
+
+# 📝 Note
+"""
+I'm so exhausted and tired for practice everyday code. May be i'm getting burnout because every code is so hard even it's in 8 kyu. So, i must take a breath and rest.
+"""
